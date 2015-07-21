29244ee329de594c70aa86b91bc5ea5782c5403d
MKS-students <mks.students@hackreactor.com>
2015-07-20 20:56:00 -0700

    [Cleanup] Removed ugly comments

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,8 +1,5 @@
-var Db = require('tingodb')().Db,
-    assert = require('assert');
-
+var Db = require('tingodb')().Db;
 var db = new Db('./', {});
-// Fetch a collection to insert document into
 var collection = db.collection("message_store");
 
 exports.requestHandler = function(request, response) {
@@ -66,39 +63,16 @@ exports.requestHandler = function(request, response) {
     handler(request,response);
     return;
   }
-  // The outgoing status.
+
+
+  // 404
   var statusCode = 404;
 
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
   headers['Content-Type'] = "text/html";
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
   response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
   response.end("404: Not Found");
 };
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
